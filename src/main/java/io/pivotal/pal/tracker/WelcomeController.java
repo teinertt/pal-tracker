a0f4e032440e119d1acfe949984d3307b65e6a60
teinertt <teinertt@gmail.com>
2020-05-15 18:55:23 -0400

    initial commit

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,22 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.event.WindowListener;
+
+@RestController
+public class WelcomeController {
+
+    private final String message;
+
+    public WelcomeController (@Value("${welcome.message}") String message){
+        this.message = message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return message;
+    }
+}
